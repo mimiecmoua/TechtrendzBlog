fd47ff46d41bc4a34bf6081408184f2717f06ffa
Emilie <dionatsumi@gmail.com>
2023-10-04 17:18:39 +0200

    require articles

--- lib/article.php
+++ lib/article.php
@@ -4,3 +4,24 @@ $articles = [
     ["title" => "React ou React Native", "content" => "REACT Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", "image" => "2-react-vs-react-native.jpg"],
     ["title" => "Les meilleurs outils devops", "content" => "DEVOPS Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", "image" => "3-devops.png"],
 ];
+
+
+function getArticles(PDO $pdo, int $limit = null): array
+{
+    $sql = "SELECT * FROM articles ORDER BY id DESC";
+    if ($limit) {
+        $sql .= " LIMIT :limit";
+    }
+
+
+    $query = $pdo->prepare($sql);
+
+    if ($limit) {
+        $query->bindValue(":limit", $limit, PDO::PARAM_INT);
+    }
+
+    $query->execute();
+    $articles = $query->fetchAll(PDO::FETCH_ASSOC);
+
+    return $articles;
+}
--- templates/article_part.php
+++ templates/article_part.php
@@ -1,8 +1,20 @@
+<?php
+if ($article["image"] === null) {
+    $imagePath = _ASSETS_IMAGES_FOLDER_ . "default-article.jpg";
+} else {
+    $imagePath = _ARTICLES_IMAGES_FOLDER_ . htmlentities($article["image"]);
+}
+
+
+?>
+
+
+
 <div class="col-md-4 my-2">
     <div class="card">
-        <img src="./uploads/articles/<?= $article["image"] ?>" class="card-img-top" alt="<?= $article["title"] ?>">
+        <img src="<?= $imagePath ?>" class="card-img-top" alt="<?= htmlentities($article["title"]) ?>">
         <div class="card-body">
-            <h5 class="card-title"><?= $article["title"] ?></h5>
+            <h5 class="card-title"><?= htmlentities($article["title"]) ?></h5>
             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
             <a href="actualite.php?id=<?= $key ?>" class="btn btn-primary">Lire la suite</a>
         </div>
