b8305d59d8083362e7033605e20144c09f086c5b
Emilie <dionatsumi@gmail.com>
2023-10-05 18:48:19 +0200

    authentification

--- lib/config.php
+++ lib/config.php
@@ -1,5 +1,5 @@
 <?php
-
+define("_DOMAIN_", ".TECHTRENDZ");
 define("_DB_SERVER_", "localhost");
 define("_DB_NAME_", "studi_techtrendz_live");
 define("_DB_USER_", "root");
---
+++ lib/session.php
@@ -0,0 +1,22 @@
+<?php
+
+session_set_cookie_params([
+    'lifetime' => 3600,
+    'path' => '/',
+    'domain' => _DOMAIN_,
+    /*'secure' => true,*/
+    'httponly' => true
+]);
+
+session_start();
+
+function adminOnly()
+{
+    if (!isset($_SESSION['user'])) {
+        // Rediriger vers login
+        header('location: ../login.php');
+    } else if ($_SESSION['user']['role'] != 'admin') {
+        // Rediriger vers accueil
+        header('location: ../index.php');
+    }
+}
